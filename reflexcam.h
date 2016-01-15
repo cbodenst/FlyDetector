fb9701ecc446f7b570421cfb10d83ff9c3a75697
Ewa Osekowska <ewa.osekowska@gmail.com>
2016-01-15 11:19:47 +0100

    refactored reflexcam

--- reflexcam.h
+++ reflexcam.h
@@ -1,22 +1,24 @@
 #ifndef REFLEXCAM_H
 #define REFLEXCAM_H
 
-#include "flycam.h"
-
 #include <gphoto2/gphoto2-camera.h>
+#include <opencv2/opencv.hpp>
 
-class ReflexCam : public FlyCam
+#include "cam.h"
+
+class ReflexCam : public Cam
 {
+protected:
     Camera* cam;
     GPContext* context;
+
 public:
     ReflexCam();
-    virtual ~ReflexCam();
+
     virtual bool getImage(cv::Mat& mat);
-    virtual bool setFocus(int);
-protected:
-    void error_func(GPContext *context, const char *format, va_list args, void *data);
-    void message_func(GPContext *context, const char *format, va_list args, void *data);
+    virtual inline bool setFocus(int) { return false; }
+
+    virtual ~ReflexCam();
 };
 
 #endif // REFLEXCAM_H
