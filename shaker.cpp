79e130ed85b444e0ed7a7052222ac3367dcb724f
Ewa Osekowska <ewa.osekowska@gmail.com>
2016-01-15 13:43:43 +0100

    Shaker is now threadsafe

--- shaker.cpp
+++ shaker.cpp
@@ -1,30 +1,30 @@
 #include "shaker.h"
 
 #include <iostream>
-#include <unistd.h>
+#include <mutex>
 
-unsigned char Shaker::ON[] = {0xFF, 0x01};
+unsigned char Shaker::ON[]  = {0xFF, 0x01};
 unsigned char Shaker::OFF[] = {0xFD, 0x01};
 
 Shaker::Shaker()
 {
-    int error = libusb_init(NULL);
+    int error = libusb_init(nullptr);
     if (error != 0)
     {
         std::cerr  << "Could not initialize lib" << std::endl;
         return;
     }
+    this->device = libusb_open_device_with_vid_pid(nullptr, VID, PID);
 
-    this->device = libusb_open_device_with_vid_pid(NULL,VID,PID);
-
-    if (this->device == NULL)
+    if (this->device == nullptr)
     {
         std::cerr  << "Could not find device" << std::endl;
         return;
     }
-    if (libusb_kernel_driver_active(this->device,0))
+
+    if (libusb_kernel_driver_active(this->device, 0))
     {
-        error = libusb_detach_kernel_driver(this->device,0);
+        error = libusb_detach_kernel_driver(this->device, 0);
 
         if (error != 0)
         {
@@ -32,29 +32,56 @@ Shaker::Shaker()
             return;
         }
     }
-    error = libusb_claim_interface(this->device,0);
 
+    error = libusb_claim_interface(this->device, 0);
     if (error != 0)
     {
         std::cerr  << "Could not claim interface" << std::endl;
         return;
     }
-
 }
 
-
-void Shaker::_shakeFor(int millisecs)
+void Shaker::shake()
 {
     this->start();
-    usleep(millisecs * 1000);
+    while (true) {
+        this->mutex.lock();
+        bool need_to_shake = this->end > clock_t::now();
+        this->mutex.unlock();
+
+        if (!need_to_shake)
+        {
+            break;
+        }
+        std::this_thread::sleep_until(this->end);
+    }
     this->stop();
 }
 
 
 void Shaker::shakeFor(int millisecs)
-{   if(this->t.joinable())
-        this->t.join();
-    this->t = std::thread(&Shaker::_shakeFor, this, millisecs);
+{   
+    // shaker is running or ran
+    if (this->thread.joinable())
+    {
+        this->mutex.lock()
+        // end of shaking is in future - still running, increase end
+        if (this->end >= clock_t::now())
+        {
+            this->end += std::chrono::milliseconds(millisecs);
+            this->mutex.unlock();
+            return;
+        }
+        // shaker ran is in the past, join old thread and create a new one
+        else
+        {
+            this->thread.join();
+        }
+    }
+    // SHAKE IT BABY!
+    this->end = clock_t now + std::chrono::milliseconds(millisecs);
+    this->thread = std::thread(&Shaker::_shakeFor, this);
+    this->mutex.unlock();
 }
 
 void Shaker::start()
@@ -62,7 +89,6 @@ void Shaker::start()
      int written;
 
      written = libusb_control_transfer(device, LIBUSB_REQUEST_TYPE_CLASS, LIBUSB_REQUEST_SET_CONFIGURATION, LIBUSB_REQUEST_SET_FEATURE, 0, Shaker::ON, Shaker::MSG_LEN, Shaker::TIMEOUT);
-
      if (written != Shaker::MSG_LEN)
      {
          std::cerr  << "Could not write data" << std::endl;
@@ -75,7 +101,6 @@ void Shaker::stop()
      int written;
 
      written = libusb_control_transfer(device, LIBUSB_REQUEST_TYPE_CLASS, LIBUSB_REQUEST_SET_CONFIGURATION, LIBUSB_REQUEST_SET_FEATURE, 0, Shaker::OFF, Shaker::MSG_LEN, Shaker::TIMEOUT);
-
      if (written != Shaker::MSG_LEN)
      {
          std::cerr  << "Could not write data" << std::endl;
@@ -84,7 +109,7 @@ void Shaker::stop()
 
 Shaker::~Shaker()
 {
-
+    this->stop();
     libusb_release_interface(this->device, 0);
     libusb_close(this->device);
     libusb_exit(NULL);
--- shaker.h
+++ shaker.h
@@ -1,30 +1,45 @@
 #ifndef SHAKER_H
 #define SHAKER_H
 
-
+#include <chrono>
 #include <libusb-1.0/libusb.h>
+#include <mutex>
 #include <thread>
+
+typedef std::chrono::high_resolution_clock clock_t;
+typedef std::chrono::time_point<clock_t> timepoint_t;
+
 class Shaker
 {
 protected:
+    /* USB on/off packages */
     static unsigned char ON[];
     static unsigned char OFF[];
+
+    /* USB relay constants */
     static const int TIMEOUT = 5000;
     static const int MSG_LEN = 2;
-    static const int VID = 0x16c0;
-    static const int PID = 0x05df;
+    static const int VID     = 0x16c0;
+    static const int PID     = 0x05df;
 
-    std::thread t;
+    /* synchronization and threading for shaker thread */
+    std::mutex  mutex;
+    std::thread thread;
+    timepoint_t end;
+
+    /* USB device */
     libusb_device_handle* device;
-    void _shakeFor(int millisecs);
+
+    void shake();
+
 public:
     Shaker();
-    ~Shaker();
 
     void shakeFor(int millisecs=5000);
     void start();
     void stop();
-
+    
+    ~Shaker();
 };
 
 #endif // SHAKER_H
