6fb607c5c5ca7d87b8dd920f7906486a87f34c87
Ewa Osekowska <ewa.osekowska@gmail.com>
2016-01-15 10:46:50 +0100

    * Renamed FlyCam to Cam\n * Renamed SampleCam to FileCam\n * Implemented missing return value for getImage in FileCam

---
+++ cam.h
@@ -0,0 +1,18 @@
+#ifndef CAM_H
+#define CAM_H
+
+#include <opencv2/opencv.hpp>
+
+/* Abstract camera interface */
+class Cam
+{
+public:
+    bool accessable = false;
+
+    /* Reads an image from the camera into the mat parameter and returns true if successful, false otherwise */
+    virtual bool getImage(cv::Mat& mat) = 0;
+    /* Focuses the camera to the passed */
+    virtual bool setFocus(int value) = 0;
+};
+
+#endif // CAM_H
