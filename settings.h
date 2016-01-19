979d74e027590e2af615832d4c7db007d8ad71fd
Markus Goetz <murxman@gmail.com>
2016-01-19 11:58:09 +0100

    changed the static constants in the settings to be of type Duration

--- settings.h
+++ settings.h
@@ -4,22 +4,22 @@
 #include <chrono>
 #include <string>
 
-struct SETTINGS
+struct Settings
 {
     static const std::string OUTPUT_PATH = "results";
     static const std::string OUTPUT_FILE = "results.csv";
     static const bool        SAVE_IMAGES = true;
+    static const std::string TIME_FORMAT = "%Y-%m-%d %H:%M:%S";
 
-    static const int VEILS           = 20;
-    static const int MEASURMENT_TIME = 60; // seconds
-    static const int EPSILON         = 5;
-    static const int MIN_POINTS      = 50;
-    static const int PIXELS_PER_FLY  = 300;
-    static const int THRESHOLD       = 100;
+    static const int EPSILON             = 5;
+    static const int MIN_POINTS          = 50;
+    static const int PIXELS_PER_FLY      = 300;
+    static const int THRESHOLD           = 100;
+    static const int VEILS               = 20;
 
-    static const Duration ROUND_TIME = Seconds(15);
-    static const Duration SHAKE_LEAD = Seconds(10);
-    static const Duration SHAKE_TIME = Seconds(5);
+    static const Duration ROUND_TIME     = Seconds(60);
+    static const Duration SHAKE_LEAD     = Seconds(10);
+    static const Duration SHAKE_TIME     = Seconds(5);
 };
 
 #endif // SETTINGS_H
