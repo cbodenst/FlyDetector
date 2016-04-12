5a851589cf9e8d16eef31b3160ba753a21eca715
cbodenst <cbodenstein@gmx.de>
2016-04-12 16:24:01 +0200

    File fix

--- API/python/test.py
+++ API/python/test.py
@@ -6,14 +6,30 @@ from PIL import Image
 fc = FlyCounter()
 flies = h5.File("/home/cboden/Extern/Data/Flies/flies2.h5")
 images = flies["data"]
+label = flies["label"]
 fc.setVialSize(150)
-fc.setThreshold(100)
-fc.setEpsilon(5)
-fc.setMinPoints(40)
-fc.setPixelsPerFly(200)
-for i in range(1):#images.shape[0]):
-    img = np.rollaxis(images[i],0,3)
-    g = fc.generateThresholdImage(img)
-    Image.fromarray(g).show()
-    f = fc.count(img)
-    print(f)
+out = h5.File("results.h5")
+results = out.create_dataset('run', (0,5), maxshape=(None,5),dtype='f8')
+out.close()
+for t in range (50,150,5):
+    for e in range(2,20):
+        for m in range(10,100,2):
+            for p in range(50, 400,10):
+                fc.setThreshold(t)
+                fc.setEpsilon(e)
+                fc.setMinPoints(m)
+                fc.setPixelsPerFly(p)
+                mse = 0.0;
+                for i in range(images.shape[0]):
+                    img = np.rollaxis(images[i],0,3).copy()
+                    f = fc.count(img)
+                    mse += (f - label[i][0])**2
+
+                mse /= images.shape[0]
+                out = h5.File("results.h5")
+                results = out["run"]
+                idx = results.shape[0]
+                results.resize((idx+1,5))
+                results[idx] = np.array([t,e,m,p,mse])
+                out.close()
+                print("Threshold: %d Epsilon: %d MinPts: %d PPF: %d MSE:%f"%(t,e,m,p,mse))
