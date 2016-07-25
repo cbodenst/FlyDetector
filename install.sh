ba229d877f7da2659447554b3095129f69eb2d29
cbodenst <cbodenstein@gmx.de>
2016-07-25 16:16:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git pull
 sudo cp fly-shaker.rules /etc/udev/rules.d/fly-shaker.rules
 mkdir -p build
 cd build
-qmake ../FlyDetector.pro -r -spec linux-g++
+qmake-qt5 ../FlyDetector.pro -r -spec linux-g++
 make
 mkdir -p ../bin
 cp FlyDetector ../bin/FlyDetector
